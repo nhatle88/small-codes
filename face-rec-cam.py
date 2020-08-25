22174736e0749c442b2f5687b1121d7ed94b89dc
Nhat Le <lehoangnhat88@gmail.com>
2020-08-25 13:16:50 +0700

    Update face-rec-cam.py

--- face-rec-cam.py
+++ face-rec-cam.py
@@ -2,4 +2,4 @@
 python src/align_dataset_mtcnn.py  Dataset/FaceData/raw Dataset/FaceData/processed --image_size 160 --margin 32  --random_order --gpu_memory_fraction 0.25
 
 # Train model
-python src/classifier.py TRAIN Dataset/FaceData/processed Models/20180408-102900.pb Models/facemodel.pkl --batch_size 1000
+python src/classifier.py TRAIN Dataset/FaceData/processed Models/20180402-114759.pb Models/facemodel.pkl --batch_size 1000
